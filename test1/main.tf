bd3b2ca163d88a8395468fa62923c18315dac767
James Dumont <j.dumont@veebaze.com>
2019-11-04 14:33:13 +0100

    Use another subscription

--- test1/main.tf
+++ test1/main.tf
@@ -1,7 +1,7 @@
 #Set the terraform backend
 terraform {
   backend "azurerm" {
-    storage_account_name = "infrsdbx1vpcjdld1"
+    storage_account_name = "infrasdbx1vpcjdld1"
     container_name       = "tfstate"
     key                  = "Az-LoadBalancer.test.tfstate"
     resource_group_name  = "infr-jdld-noprd-rg1"
