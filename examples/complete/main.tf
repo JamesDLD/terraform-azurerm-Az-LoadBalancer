d7643df014318bf31188a6bc58df9a50b26c07ee
DUMONT, James <jadumont@tf1.fr>
2022-11-13 12:30:49 +0100

    Upgrade terraform

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
-  version         = "~> 2.0"
+  version         = "~> 3.31"
   features {}
 }
 
